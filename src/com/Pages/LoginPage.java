57ace37731db79aed83475d82601abba7d9c6e27
azizuliam <mhoque.iam@gmail.com>
2024-06-04 10:08:57 -0400

    update the login page class

--- src/com/Pages/LoginPage.java
+++ src/com/Pages/LoginPage.java
@@ -1,5 +1,12 @@
 package com.Pages;
 
 public class LoginPage {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Help");
+		String un ="admin";
+		String pwd="Admin";
+		System.out.println("loging in ...............");
+	}
 
 }
