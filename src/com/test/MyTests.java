1cce46cba8a270fd6efb89f8c2119e0940740f97
azizuliam <mhoque.iam@gmail.com>
2024-06-04 14:38:11 -0400

    update mytest class add name

---
+++ src/com/test/MyTests.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class MyTests {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String name="azizuliam";
+	System.out.println(name);	
+	
+	}
+
+}
